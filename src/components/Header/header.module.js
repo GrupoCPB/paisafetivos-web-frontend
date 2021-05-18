720cb3216424bfd21d844129ab5eb756fb30332b
Lucas Andrius de Oliveira <74381770+LucasAndrius@users.noreply.github.com>
2021-05-18 10:22:04 -0300

    criando Route.js responsável pelo roteamento

---
+++ src/components/Header/header.module.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const Container = styled.section`
+background-color:#39aae1;
+
+max-width:71.25rem;
+height:6.25rem;
+
+`;
---
+++ src/components/Header/index.js
@@ -0,0 +1,9 @@
+import { Container } from "./header.module";
+
+export function Header(){
+    return(
+        <Container>
+         <a href="/sobre">sobre</a>
+        </Container>
+    );
+}
