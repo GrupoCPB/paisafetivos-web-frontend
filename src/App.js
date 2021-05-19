24b9fb5a1a3e8120fa6005dc2306ba2dfccdc5d1
Lucas Andrius de Oliveira <74381770+LucasAndrius@users.noreply.github.com>
2021-05-19 17:03:53 -0300

    definindo footer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import './App.css';
 import { BrowserRouter } from 'react-router-dom';
+
 import { Header } from './components/Header';
+import { Footer } from './components/Footer';
+
 import Routes from './Routes';
 
 function App() {
@@ -8,6 +11,7 @@ function App() {
     <BrowserRouter>
       <Header/>
       <Routes/>
+      <Footer/>
     </BrowserRouter>
   );
 }
--- src/pages/Home/works.js
+++ src/pages/Home/works.js
@@ -3,7 +3,8 @@ import styled from 'styled-components';
 
 
 export const Works = styled.section`
-margin-top:-150px;
+    margin-top:-150px;
+    padding-bottom:58px;
     text-align:center;
     background-color:#f4f4f4;
     
@@ -29,7 +30,7 @@ margin-top:-150px;
                 margin:0px 16px;
                 background-color:#ffffff;
                 border-radius:7px;
-                filter: drop-shadow(1px 0px 1px rgba(0,0,0,0.25));
+                filter: drop-shadow(1px 2px 1px rgba(0,0,0,0.25));
                 img{
                     height:110px;
                     padding-top:50px;
@@ -41,6 +42,7 @@ margin-top:-150px;
                     font-weight:600;
                 }
                 p{
+                    font-size:18px;
                     padding:7px 40px;
                 }
             }
