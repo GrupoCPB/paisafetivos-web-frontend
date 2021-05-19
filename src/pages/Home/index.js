1ac8c53b457d618bb25dae4cfe6296d3c1b8772b
Lucas Andrius de Oliveira <74381770+LucasAndrius@users.noreply.github.com>
2021-05-19 10:57:38 -0300

    definindo sessão about

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,21 +1,41 @@
 import React from 'react';
 import { Helmet } from "react-helmet";
+
 import { Container } from "./home.module";
+
+
+import flecha from '../../assets/flecha-direita.png';
 
 export function Home (){
     return(
         <Container>
             <Helmet title="Pais Afetivos | Home" />
             <div className="section-donate">
-                <div className="about-left">
+                <div className="donate-left">
                     <h1>Conectando <br/>pessoas<br/> maravilhosas</h1>
                     <p>Atuamos desde 2009 acolhendo pessoas LGBTQ+ que são vítimas de violência. Nos ajude a transformar a vida dessas pessoas.</p>
                     <a href="#">Faça sua doação</a>
                 </div>
-                <div className="about-right">
+                <div className="donate-right">
                     <div className="background"></div>
                 </div>
             </div>
+            <main className="about">
+                <div className="about-photo">
+                    
+                </div>
+                <div className="about-area">
+                    <h2>Quem somos</h2>
+                    <div className="about-area-text">
+                        <p>
+                            A PaisAfetivos é uma organização sem fins lucrativos que atua por redes de contatos televirtuais em todo o Brasil. Viabilizamos apoiar e acolher pessoas que tenham seus direitos fundamentais violados por conta de preconceitos.                        
+                        </p>
+                        <div className="about-link">
+                            <a href="#">Saiba mais <img src={flecha}/></a>
+                        </div>
+                    </div>
+                </div>                
+            </main>
         </Container>
     );
 }
