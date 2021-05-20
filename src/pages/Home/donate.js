bd2e011e94ee4b5e570eab8f4300c93037c0d56f
Lucas Andrius de Oliveira <74381770+LucasAndrius@users.noreply.github.com>
2021-05-20 11:00:26 -0300

    dividindo css por sessões

---
+++ src/pages/Home/donate.js
@@ -0,0 +1,69 @@
+import styled from 'styled-components';
+
+import background from '../../assets/connectando-pessoas.png';
+
+export const Donate = styled.section`
+    height:41.25rem;
+    width:100%;
+    overflow:hidden;
+
+
+    display:flex;
+    justify-content:center;
+    align-items:center;
+
+    background-color:#FF40A7;
+
+
+
+div{
+    width:33.75rem;
+}
+
+.donate-left{
+    color:#f1f1f1;
+    h1{
+        font-weight:800;
+        font-size:4.5rem;
+    }
+    p{
+        margin-top:-16px;
+        padding-bottom:1rem;
+        max-width:27.5rem;
+    }
+    a{
+        font-size:20px;
+        padding:19px 59px;
+        border-radius:10px;
+
+        text-decoration:none;
+        color:#f1f1f1;
+        background-color:#0F6FFF;
+
+        transition:all ease 0.2s;
+        &:hover{
+            filter:brightness(120%);
+        }
+    }
+}
+.donate-right{
+
+}
+
+.background{
+    height:43.75rem;
+    width:43.75rem;
+    background-image:url(${background});
+    background-size:cover;
+    background-position:right;
+    border-top-right-radius:none;
+    border-top-left-radius:50%;
+    border-bottom-right-radius:50%;
+    border-bottom-left-radius:50%;
+
+    
+    
+    transform: translate(+30px,-40px);
+
+}
+`;
---
+++ src/pages/Home/helping.js
@@ -0,0 +1,53 @@
+import styled from 'styled-components';
+
+import helping from '../../assets/ajuda.png';
+
+export const Helping = styled.section`
+
+height:38.125rem;
+width:100%;
+
+margin-top:-50px;
+
+display:flex;
+justify-content:center;
+
+.helping-photo{
+    height:400px;
+    width:590px;
+    background-size:cover;
+    background-position:center;
+    background-image:url(${helping});
+    background-repeat:no-repeat;
+}
+
+.helping-area{       
+    
+    h2{
+        font-size:40px;
+        font-weight:600;
+    }
+
+    .helping-area-text{
+        p{
+            max-width:400px;
+            padding-bottom:32px;
+        }
+
+        a{
+            text-decoration:none;
+            color:#2D9CDB;
+            font-weight:700;
+
+            &:hover{
+                text-decoration:underline;                    
+            }
+            img{
+                height:10px;
+                margin-left:3px;
+            }
+        }
+    }        
+}
+}
+`
