eb39af116152b478245d4b7f3d75600027901c35
igson <igson660@gmail.com>
2021-12-04 13:31:25 -0500

    baixa atualizaçoes da main

---
+++ src/tests/home.test.js
@@ -0,0 +1,7 @@
+import { render, screen } from '@testing-library/react';
+import Home from '../pages/Home';
+
+describe('Testes do Componente Header', () => {
+  test('Verifica a renderização do componente', () => {
+  });
+});
