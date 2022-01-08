867b179cde65b3136c2785301d04099d77edd569
igson <igson660@gmail.com>
2022-01-08 10:18:07 -0500

    criaçao das rotas equipe, tranparencia, doaçao, como-ajudar e contato

--- src/Routes.js
+++ src/Routes.js
@@ -4,14 +4,25 @@ import { Home } from './pages/Home.js';
 import { Partners } from './pages/Partners.js';
 import NeedHelp from './pages/NeedHelp.js';
 import About from './pages/About.js';
+import Team from './pages/Team.js';
+import Transparency from './pages/Transparency.js';
+import Donation from './pages/Donation.js';
+import HowHelp from './pages/HowHelp.js';
+import Contact from './pages/Contact.js';
+
 
 const routers = () => {
   return (
     <Switch>
       <Route exact path="/" component={Home} />
-      <Route exact path="/parceiros" component={Partners} />
-      <Route exact path="/ajuda" component={NeedHelp} />
+      <Route path="/parceiros" component={Partners} />
+      <Route path="/preciso-ajudar" component={NeedHelp} />
       <Route path="/sobre" component={About} />
+      <Route path="/equipe" component={Team} />
+      <Route path="/transparencia" component={Transparency} />
+      <Route path="/doacao" component={Donation} />
+      <Route path="/como-ajudar" component={HowHelp} />
+      <Route path="/contato" component={Contact} />
     </Switch>
   );
 };
