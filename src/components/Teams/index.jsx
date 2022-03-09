56d99f26f49d7fb52ae475cc05b32f3335636bc8
Cristina <cristoshiwassaki@gmail.com>
2022-03-09 12:24:34 -0300

    criando componente peopleInfo

--- src/components/Teams/index.jsx
+++ src/components/Teams/index.jsx
@@ -6,7 +6,21 @@ import coordinator from 'assets/Mariana.png'
 import director from 'assets/raphael.png'
 import group from 'assets/equipe.png'
 
-
+function PeopleInfo(props) {
+  return (
+    <Container>
+      <Avatar 
+        src={props.image}
+        sx={{ width: 150, height: 150 }}
+      />
+      <Typography
+        variant='h6'>{props.name}</Typography>
+      <Typography>Função: {props.occupation}</Typography>
+      <Typography>Contrato: Voluntário</Typography>
+      <Typography>Instagram: {props.insta}</Typography>
+    </Container>
+  )
+}
 
 export function OurTeam (){
   return(
@@ -15,71 +29,38 @@ export function OurTeam (){
           <div>
             <h2>Nossa Equipe</h2>
             <p>
-              Essas são as pessoas responsáveis pela nossa ONG. Além delas, 
+              Essas são as pessoas responsáveis pela nossa ONG. <br />Além delas, 
               contamos com diversos voluntários nas nossas equipes multidisciplinares, 
-              como psicólogos, assistentes sociais, advogados, agentes de atendimento, entre outros. </p>
+              como psicólogos, assistentes sociais, advogados, <br />agentes de atendimento, entre outros. </p>
             <p>Se você gostaria de fazer parte de nossa equipe, entre em contato conosco.</p>
           </div>
           <img src={group} alt='imagem de uma equipe'/>
         </div>
       <section className='executive-people'>
           <h4>Administrativo</h4>
-        <Stack direction='row'>
-          <Container>
-            <Avatar 
-              src={director}
-              sx={{ width: 160, height: 160}}
+          <Stack direction='row'>
+            <PeopleInfo 
+              image={director}
+              name="Raphael América"
+              occupation="Diretor Geral"
+              insta="@oamericaraphael" 
             />
-                <Typography
-                  variant='h6'
-                >
-                    Raphael América
-                </Typography>
-                <Typography>
-                    Função: Diretor Geral
-                </Typography>
-                <Typography>
-                    Contrato: Voluntário
-                </Typography>
-                <Typography>
-                    Instagram: @oamericaraphael
-                </Typography>
-              </Container>
-            <Container>
-              <Avatar
-                src={executive}
-                sx={{ width: 160, height:160}}
-              />
-              <Typography
-                variant='h6'
-              >
-                Alessandra Keiko
-              </Typography>
-              <Typography>
-              Função: Diretora Executiva
-              </Typography>
-              <Typography>
-                Contrato: Voluntária
-              </Typography>
-              <Typography>
-                Instagram: @alessandrakeikoaoki
-              </Typography>
-            </Container>
-               
-          <Container>
-            <Avatar
-              src={coordinator}
-              sx= {{ width: 160, height: 160}}
+
+            <PeopleInfo 
+              image={executive}
+              name="Alessandra Keiko"
+              occupation='Diretora Executiva'
+              insta="@alessandrakeikoaoki"            
             />
-            <Typography
-              variant='h6'
-            >Mariana Gidra
-            </Typography>
-            <Typography>Função: Coord. Multidisciplinar</Typography>
-            <Typography>Contrato: Voluntária</Typography>
-            <Typography>Instagram: @marianagidra</Typography>
-          </Container>  
-        </Stack>
+
+            <PeopleInfo
+              image={coordinator}
+              name="Mariana Gidra"
+              occupation='Coord. Multidisciplinar'
+              insta='@marianagidra'
+            />
+          </Stack>
+
         <article className="finance-people">
           <h4>Financeiro</h4>
             <Avatar
@@ -92,24 +73,24 @@ export function OurTeam (){
         </article>
         <article className="hr-people">
           <h4>Recursos Humanos</h4>
-          <Avatar
-          src={coordinator}
-          sx={{ width: 160, height: 160 }} />
-          
-          <Typography>Mariana Gidra</Typography>
-          <Typography>Função: Coord. de RH</Typography>
-          <Typography>Contrato: Voluntária</Typography>
-          <Typography>Instagram: @marianagidra</Typography>
+          <PeopleInfo
+              image={coordinator}
+              name="Mariana Gidra"
+              occupation='Coord. de RH'
+              insta='@marianagidra'
+            />
         </article>
         <article className='marketing-people'>
           <h4>Marketing</h4>
-          <Avatar
-            sx={{ width: 100, height: 100}}
-          >JL</Avatar>
-          <Typography variant='h6'>Julia Leonello</Typography>
-          <Typography>Função: Coord. Social Media</Typography>
-          <Typography>Contrato: Voluntária</Typography>
-          <Typography>Instagram: @juleonello</Typography>
+          <Container>
+            <Avatar
+              sx={{ width: 100, height: 100}}
+            >JL</Avatar>
+            <Typography variant='h6'>Julia Leonello</Typography>
+            <Typography>Função: Coord. Social Media</Typography>
+            <Typography>Contrato: Voluntária</Typography>
+            <Typography>Instagram: @juleonello</Typography>
+          </Container>
         </article>
       </section>
     </OurTeamStyled>
