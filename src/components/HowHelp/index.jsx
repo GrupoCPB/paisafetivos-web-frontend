8779ac7a416ded9f0f6078d8e2ab2d7d0e6a2c43
Cristina <cristoshiwassaki@gmail.com>
2022-04-06 11:44:12 -0300

    estilos

--- src/components/HowHelp/index.jsx
+++ src/components/HowHelp/index.jsx
@@ -2,29 +2,18 @@ import helpImg from 'assets/ajuda-imagem.png'
 import flag from 'assets/flag-icon.png'
 // import arrow from 'assets/flecha-direita.png'
 
-import { Typography } from '@mui/material'
+import { Stack, Typography } from '@mui/material'
 import ArrowRightAltIcon from '@mui/icons-material/ArrowRightAlt';
+import { Box } from '@mui/system';
 
 
 //deixar só a imagem da bandeira como componente estático
 
-const Topics = (props) => {
+const FlagFixed = () => {
   return(
     <div>
-        <div>
-          <img src={flag} alt="" />
-          <div>
-            <Typography variant="h4">{props.title}</Typography>
-            <a href={props.link}>
-              {props.reference} 
-              <ArrowRightAltIcon color="secondary" />
-            </a>
-          </div>
-        </div>
-        <Typography variant="body1">
-          {props.information}
-        </Typography>
-      </div>
+      <img src={flag} alt="" />
+    </div>
   )
 }
 
@@ -48,36 +37,40 @@ export function HowHelpComponent(){
         <img src={helpImg} alt="" />
       </div>
       
-      <div>
-        <Topics
-          title='Doações'
-          link='/como-ajudar'
-          reference='Para doar'
-          information='A Pais Afetivos é uma instituição sem fins lucrativos, por isso
-          precisamos de pessoas como você para continuarmos nossa missão. Você pode doar 
-          qualquer valor, isolado ou mensal, para manutenção e contribuição dos
-          projetos da PaisAfetivos, além de roupas e demais elementos que achar prudente.'
-        />
-        <Topics
-          title="Voluntário"
-          link="/como-ajudar"
-          reference="Seja um voluntário"
-          information="Você pode nos ajudar com nosso trabalho a partir do teu
-          conhecimento, profissão ou empresa. Como atuamos de forma
-          multidisciplinar todo conhecimento é bem vindo. Entre em
-          contato conosco pelo botão abaixo e nós te direcionaremos
-          para nossas áreas em demanda"
-        />
-        <Topics
-          title="Aniversário Solidário"
-          link="/como-ajudar"
-          reference='Colabore conosco'
-          information="Faça se Aniversário Solidário em qualquer lugar do mundo
-          e ajude a #PaisAfetivos
-          Você pode sugerir uma ação solidária onde cada um de seus convidados 
-          destinaria 10 reais ou mais, para as ações da ong e esta doação 
-          simbólica seria uma forma de seus amigos te presentearem em seu aniversário."
-        /> 
+      <div className="help-options">
+        <Box 
+          component="div"
+          sx={{
+            display: 'flex',
+            flexDirection: { xs: 'column', md: 'row' },
+            alignItems: 'center',
+            justifyContent: 'space-evenly',
+          }}
+        >
+          <Stack direction="row" spacing={2}>
+            <FlagFixed />
+            <Stack>
+              <Typography variant="h4">Doações</Typography>
+              <Box
+                sx={{
+                  display: 'flex'
+                }}
+              >
+                <a href="/como-ajudar">Para doar</a>
+                <ArrowRightAltIcon color="secondary" />
+              </Box>
+            </Stack>
+          </Stack>
+          <div className="help-right">
+            <Typography variant="body1">
+              A Pais Afetivos é uma instituição sem fins lucrativos, por isso<br/>
+              precisamos de pessoas como você para continuarmos nossa<br/>
+              missão. Você pode doar qualquer valor, isolado ou mensal, para<br/>
+              manutenção e contribuição dos projetos da PaisAfetivos, além<br/>
+              de roupas e demais elementos que achar prudente.
+            </Typography>
+          </div>
+        </Box>
       </div>
     </>
   )
