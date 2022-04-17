073514c1b3cd7ca50bc25848abcb49cce11f2b96
Igson <igson660@gmail.com>
2022-04-17 12:19:24 -0500

    add: componente formulário

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,4 +2,6 @@ export * from './about';
 export * from './home';
 export * from './needHelp';
 export * from './partners';
-export * from './Team';
+export * from './team/Team';
+export * from './howToHelp';
+export * from './team';
