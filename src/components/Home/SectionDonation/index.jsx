70ae9d9f46177f40c018fa180f56b22ff0948881
Igson <igson660@gmail.com>
2022-04-23 16:15:12 -0500

    add: resposividade a seção de doação

--- src/components/Home/SectionDonation/index.jsx
+++ src/components/Home/SectionDonation/index.jsx
@@ -1,92 +1,45 @@
 import React from "react";
 import Box from '@mui/material/Box';
-import Typography from '@mui/material/Typography';
 import donateImg from "assets/connectando-pessoas.png";
-import styled from "styled-components";
-import { Button } from "@mui/material";
-
-export const ButtonDonation = styled(Button)`
-  font-size: 20px;
-  padding: 19px 59px;
-  border-radius: 10px;
-  font-weight: 800;
-  line-height: 30px;
-  color: #FFFFFF;
-  background-color:  #398791;
-  margin-bottom: 40px;
-  transition: all ease 0.2s;
-  &:hover {
-    filter: brightness(120%);
-  }
-`;
-
-export const Strong = styled.strong`
-  color: #f63ebb;
-`;
-
-export const H1 = styled(Typography)`
-  color: #4B4B4B;
-  font-family: 'Manrope';
-  font-style: normal;
-  font-weight: 800;
-  font-size: 42px;
-  line-height: 57px;
-  letter-spacing: 0.0025em;
-  margin-bottom: 40px;
-  margin-top: 35px;
-`;
-
-export const Img = styled.img`
-  width: 606px;
-  height: 486.01px;
-`;
-
-export const P = styled.p`
-  font-weight: 500;
-  color: #545454;
-  font-size: 16px;
-  margin-bottom: 40px;
-`;
+import * as style from "./styled";
 
 export function SectionDonation() {
   return (
       <Box
-        mt="80px"
         display="flex"
         justifyContent="space-around"
-        bgcolor="#F8F8F8;"
+        sx={ style.sectionDonateResponsive }
       >
         <Box
           variant="div"
-          width="429px"
-          sx={{ display: { xs: "flex" }, flexDirection: { xs: "column" } , justifyContent: { xs: "center" } } }
+          sx={ style.sectionLefth }
         >
-          <H1
+          <style.H1
             variant="h1"
           >
-            Acolhemos pessoas <Strong>LGBTQ+</Strong> em situação de vulnerabilidade.
-          </H1>
-          <P>
+            Acolhemos pessoas <style.Strong>LGBTQ+</style.Strong> em situação de vulnerabilidade.
+          </style.H1>
+          <style.P>
             Atuamos desde 2009 acolhendo pessoas LGBTQ+ que são vítimas de
             violência. Nos ajude a transformar a vida dessas pessoas.
-          </P>
-          <ButtonDonation
+          </style.P>
+          <style.ButtonDonation
             variant="contained"
             sx={{ display: { xs: "none", md: "block" } }}
           >
             Faça sua doação
-          </ButtonDonation>
-          <ButtonDonation
+          </style.ButtonDonation>
+          <style.ButtonDonation
             variant="contained"
             sx={{ display: { xs: "block", md: "none" } }}
           >
             Doação
-          </ButtonDonation>
+          </style.ButtonDonation>
         </Box>
         <Box
           sx={{ display: { xs: "none", md: "block" } }}
         >
-          <Img src={ donateImg } alt="pessoas ao por do sol"></Img>
+          <style.Img src={ donateImg } alt="pessoas ao por do sol"></style.Img>
         </Box>
       </Box>
   );
--- src/components/Home/SectionDonation/styled.jsx
+++ src/components/Home/SectionDonation/styled.jsx
@@ -1,1 +1,61 @@
+import Typography from '@mui/material/Typography';
+import styled from "styled-components";
+import { Button } from "@mui/material";
+import donateImg from "assets/connectando-pessoas.png";
 
+export const ButtonDonation = styled(Button)`
+  font-size: 20px;
+  padding: 19px 59px;
+  border-radius: 10px;
+  font-weight: 800;
+  line-height: 30px;
+  color: #FFFFFF;
+  background-color:  #398791;
+  margin-bottom: 40px;
+  transition: all ease 0.2s;
+  &:hover {
+    filter: brightness(120%);
+  }
+`;
+
+export const Strong = styled.strong`
+  color: #f63ebb;
+`;
+
+export const H1 = styled(Typography)`
+  color: #4B4B4B;
+  font-family: 'Manrope';
+  font-style: normal;
+  font-weight: 800;
+  font-size: 42px;
+  line-height: 57px;
+  letter-spacing: 0.0025em;
+  margin-bottom: 40px;
+  margin-top: 35px;
+`;
+
+export const Img = styled.img`
+  width: 606px;
+  height: 486.01px;
+`;
+
+export const P = styled.p`
+  font-weight: 500;
+  color: #545454;
+  font-size: 16px;
+  margin-bottom: 40px;
+`;
+
+export const sectionLefth = {
+  display: { xs: "flex" },
+  flexDirection: { xs: "column" } ,
+  justifyContent: { xs: "center" },
+  width: { xs: "430px", md: "370px", lg: "429px"},
+}
+
+export const sectionDonateResponsive = {
+  mt: { md: "80px"},
+  backgroundImage: { xs: `url(${donateImg})`, md: "none"},
+  backgroundRepeat: "no-repeat",
+  backgroundSize: "cover"
+}
